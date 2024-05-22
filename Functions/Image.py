e4fccb933134455a001290e299bebb414b19789e
Robert <robertarustamyan2@gmail.com>
2024-05-22 16:28:34 +0400

    Modified Image.py. Added width and height for cell

--- Functions/Image.py
+++ Functions/Image.py
@@ -1,5 +1,4 @@
-import time
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image
 from abc import ABC
 import os
 from dotenv import load_dotenv
@@ -34,39 +33,39 @@ class GrayImage(ImageWork):
 
 
 class ConvertImage(ImageWork):
-    def _average_block_brightness(self, x, y, block_height,block_width) -> int:
+    def _average_block_brightness(self, x, y, block_size) -> int:
         total_brightness = 0
         count = 0
-        for i in range(y, min(self._height, y + block_height)):
-            for j in range(x, min(self._width, x + block_width)):
+        for i in range(y, min(self._height, y + block_size)):
+            for j in range(x, min(self._width, x + block_size)):
                 g1, _, _ = self._pixels[j, i]
                 total_brightness += (255 - g1)
                 count += 1
         return total_brightness // count if count > 0 else 0
 
-    def _image_to_ascii(self, block_height, block_width) -> str:
+    def _image_to_ascii(self, block_size) -> str:
         result = ''
-        for y in range(0, self._height, block_height):
-            for x in range(0, self._width,block_width):
-                pixel_brightness = self._average_block_brightness(x, y, block_height, block_width)
+        for y in range(0, self._height, block_size):
+            for x in range(0, self._width, block_size):
+                pixel_brightness = self._average_block_brightness(x, y, block_size)
                 char = get_value(pixel_brightness)
                 result += char
             result += '\n'
         return result
 
-    def get_image(self,  block_height, block_width):
-        result = self._image_to_ascii( block_height, block_width)
-        file_name = f"{self.name}-ascii-{block_height}-{block_width}.txt"
+    def get_image(self, block_size=10):
+        result = self._image_to_ascii(block_size)
+        file_name = f"{self.name}-ascii-{block_size}.txt"
         with open(f'../TestImages/{file_name}', 'w') as f:
             f.write(result)
         print(f"File with name {file_name}.txt is ready!")
 
 
-def ascii_textfile(folder_path, file_name, file_format, block_height, block_width):
+def ascii_image(folder_path, file_name, file_format):
     gray = GrayImage(f'{folder_path}/TestImages/{file_name}.{file_format}')
     gray.convert_to_gray()
     ascii_im = ConvertImage(f'{folder_path}/TestImages/{file_name}gray.jpg')
-    ascii_im.get_image(block_height, block_width)
+    ascii_im.get_image(5)
     # Deleting filegray picture after ascii creating
     if os.path.exists(f'{folder_path}/TestImages/{file_name}gray.jpg'):
         os.remove(f'{folder_path}/TestImages/{file_name}gray.jpg')
@@ -75,4 +74,10 @@ def ascii_textfile(folder_path, file_name, file_format, block_height, block_widt
 if __name__ == "__main__":
     # Getting path from .env
     folder_path = os.getenv("folder_path")
-    ascii_textfile(folder_path, '1', 'jpg',5, 3)
+    '''For making picture Gray'''
+    # I = GrayImage(f'{folder_path}/TestImages/saturn.png')
+    # I.convert_to_gray()
+
+    # C = ConvertImage(f'{folder_path}/TestImages/saturngray.jpg')
+    # C.get_image(5)
+    ascii_image(folder_path, 'test', 'jpg')
