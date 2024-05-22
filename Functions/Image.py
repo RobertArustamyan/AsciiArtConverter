575cd76e5a547d0b8097c0971a7383ccec4bc6aa
Robert <robertarustamyan2@gmail.com>
2024-05-22 14:42:26 +0400

    Created new function ascii_image that does 2 actions in one time

--- Functions/Image.py
+++ Functions/Image.py
@@ -3,6 +3,7 @@ from abc import ABC
 import os
 from dotenv import load_dotenv
 from Functions.Characters import get_value
+
 # Loading .env file data
 load_dotenv()
 
@@ -11,6 +12,8 @@ class ImageWork(ABC):
     def __init__(self, image_url: str):
         try:
             self.img = Image.open(image_url)
+            if image_url.endswith('.png'):
+                self.img = self.img.convert('RGB')
             self.name = image_url.split('/')[-1].split('.')[0]
         except FileNotFoundError:
             print("The file wasn't able to load")
@@ -30,7 +33,7 @@ class GrayImage(ImageWork):
 
 
 class ConvertImage(ImageWork):
-    def _average_block_brightness(self, x, y, block_size)->int:
+    def _average_block_brightness(self, x, y, block_size) -> int:
         total_brightness = 0
         count = 0
         for i in range(y, min(self._height, y + block_size)):
@@ -40,7 +43,7 @@ class ConvertImage(ImageWork):
                 count += 1
         return total_brightness // count if count > 0 else 0
 
-    def _image_to_ascii(self, block_size)->str:
+    def _image_to_ascii(self, block_size) -> str:
         result = ''
         for y in range(0, self._height, block_size):
             for x in range(0, self._width, block_size):
@@ -49,6 +52,7 @@ class ConvertImage(ImageWork):
                 result += char
             result += '\n'
         return result
+
     def get_image(self, block_size=10):
         result = self._image_to_ascii(block_size)
         file_name = f"{self.name}-ascii-{block_size}.txt"
@@ -56,12 +60,24 @@ class ConvertImage(ImageWork):
             f.write(result)
         print(f"File with name {file_name}.txt is ready!")
 
+
+def ascii_image(folder_path, file_name, file_format):
+    gray = GrayImage(f'{folder_path}/TestImages/{file_name}.{file_format}')
+    gray.convert_to_gray()
+    ascii_im = ConvertImage(f'{folder_path}/TestImages/{file_name}gray.jpg')
+    ascii_im.get_image(5)
+    # Deleting filegray picture after ascii creating
+    if os.path.exists(f'{folder_path}/TestImages/{file_name}gray.jpg'):
+        os.remove(f'{folder_path}/TestImages/{file_name}gray.jpg')
+
+
 if __name__ == "__main__":
     # Getting path from .env
     folder_path = os.getenv("folder_path")
-
-    # I = GrayImage(f'{folder_path}/TestImages/Rob.jpg')
+    '''For making picture Gray'''
+    # I = GrayImage(f'{folder_path}/TestImages/saturn.png')
     # I.convert_to_gray()
 
-    C = ConvertImage(f'{folder_path}/TestImages/2gray.jpg')
-    C.get_image(5)
+    # C = ConvertImage(f'{folder_path}/TestImages/saturngray.jpg')
+    # C.get_image(5)
+    ascii_image(folder_path, 'test', 'jpg')
