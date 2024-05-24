a4d9fdb3f604774017ae8f0fde10f17b3feec4d6
RobertArustamyan <robertarustamyan2@gmail.com>
2024-05-24 20:37:35 +0400

    Modified structure of Image.py

--- Functions/Image.py
+++ Functions/Image.py
@@ -1,78 +1,42 @@
-import time
-from PIL import Image, ImageDraw, ImageFont
-from abc import ABC
-import os
+from PIL import Image
 from dotenv import load_dotenv
-from Functions.Characters import get_value
+import os
 
-# Loading .env file data
 load_dotenv()
 
 
-class ImageWork(ABC):
-    def __init__(self, image_url: str):
+class Image2Ascii:
+    scale = r"$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+    def __init__(self, path):
         try:
-            self.img = Image.open(image_url)
-            if image_url.endswith('.png'):
-                self.img = self.img.convert('RGB')
-            self.name = image_url.split('/')[-1].split('.')[0]
+            self._image = Image.open(path)
         except FileNotFoundError:
-            print("The file wasn't able to load")
-        self._width, self._height = self.img.size
-        self._pixels = self.img.load()
+            print(f"There is no image in {path}")
 
+    def resize_image(self, new_width=200):
+        width, height = self._image.size
+        factor = height / width
+        new_height = int(factor * new_width)
+        self._image = self._image.resize((new_width, new_height))
 
-class GrayImage(ImageWork):
-    def convert_to_gray(self) -> None:
-        for y in range(self._height):
-            for x in range(self._width):
-                r, g, b = self._pixels[x, y]
-                gray = int(0.299 * r + 0.587 * g + 0.114 * b)
-                self._pixels[x, y] = (gray, gray, gray)
+    def turn_to_gray(self):
+        self._image = self._image.convert('L')
 
-        self.img.save(f"../TestImages/{self.name}gray.jpg")
+    def pixels_to_ascii(self):
+        pixels = self._image.getdata()
+        characters = "".join([self.scale[pixel * (len(self.scale) - 1) // 255] for pixel in pixels])
+        return (characters)
 
+    def convert(self, new_width):
+        self.resize_image(new_width)
+        self.turn_to_gray()
+        new_image_str = self.pixels_to_ascii()
+        pixel_count = len(new_image_str)
+        ascii_image = '\n'.join(new_image_str[i:(i + new_width)] for i in range(0, pixel_count, new_width))
+        with open('Test.txt', 'w') as f:
+            f.write(ascii_image)
 
-class ConvertImage(ImageWork):
-    def _average_block_brightness(self, x, y, block_height,block_width) -> int:
-        total_brightness = 0
-        count = 0
-        for i in range(y, min(self._height, y + block_height)):
-            for j in range(x, min(self._width, x + block_width)):
-                g1, _, _ = self._pixels[j, i]
-                total_brightness += (255 - g1)
-                count += 1
-        return total_brightness // count if count > 0 else 0
-
-    def _image_to_ascii(self, block_height, block_width) -> str:
-        result = ''
-        for y in range(0, self._height, block_height):
-            for x in range(0, self._width,block_width):
-                pixel_brightness = self._average_block_brightness(x, y, block_height, block_width)
-                char = get_value(pixel_brightness)
-                result += char
-            result += '\n'
-        return result
-
-    def get_image(self,  block_height, block_width):
-        result = self._image_to_ascii( block_height, block_width)
-        file_name = f"{self.name}-ascii-{block_height}-{block_width}.txt"
-        with open(f'../TestImages/{file_name}', 'w') as f:
-            f.write(result)
-        print(f"File with name {file_name}.txt is ready!")
-
-
-def ascii_textfile(folder_path, file_name, file_format, block_height, block_width):
-    gray = GrayImage(f'{folder_path}/TestImages/{file_name}.{file_format}')
-    gray.convert_to_gray()
-    ascii_im = ConvertImage(f'{folder_path}/TestImages/{file_name}gray.jpg')
-    ascii_im.get_image(block_height, block_width)
-    # Deleting filegray picture after ascii creating
-    if os.path.exists(f'{folder_path}/TestImages/{file_name}gray.jpg'):
-        os.remove(f'{folder_path}/TestImages/{file_name}gray.jpg')
-
-
-if __name__ == "__main__":
-    # Getting path from .env
-    folder_path = os.getenv("folder_path")
-    ascii_textfile(folder_path, '1', 'jpg',5, 3)
+if __name__ == '__main__':
+    file_path = f"{os.getenv('folder_path')}/TestImages/2.jpg"
+    a = Image2Ascii(file_path)
+    a.convert(1000)
