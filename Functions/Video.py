1ddaca3ea3305bdc1833402966372195547c025e
RobertArustamyan <robertarustamyan2@gmail.com>
2024-05-26 11:22:47 +0400

    Finished Video.py

--- Functions/Video.py
+++ Functions/Video.py
@@ -1,3 +1,6 @@
+import PIL.Image
+import moviepy.editor
+
 from Functions.Image import Image2Ascii
 from moviepy.editor import ImageSequenceClip, VideoFileClip
 from dotenv import load_dotenv
@@ -13,6 +16,10 @@ class Video2Ascii:
         self.check_file()
 
     def check_file(self):
+        '''
+        Checks the file existence
+        :return: Expectation if file is not founc
+        '''
         try:
             file_extension = os.path.split(self.path)[1].lower().split('.')[1]
             if file_extension not in ['mp4']:
@@ -20,20 +27,36 @@ class Video2Ascii:
         except FileNotFoundError:
             print(f"The file provided is not in the necessary format")
 
-    def extract_frames(self, fps=5):
+    def extract_frames(self, fps: int = 5) -> list:
+        '''
+        Gets frames from the video
+        :param fps: Frames per second
+        :return: Returns list of frames
+        '''
         clip = VideoFileClip(self.path)
         frames = [frame for frame in clip.iter_frames(fps)]
         clip.close()
         return frames
 
-    def convert_frame(self, frame, new_width):
+    def convert_frame(self, frame, new_width) -> PIL.Image.Image:
+        '''
+        Converts frame into PIL.Image
+        :param frame: HxNxf format np.array
+        :param new_width: width of new video
+        :return:
+        '''
         image = Image.fromarray(frame)
         temp_path = f"{os.getenv('folder_path')}/TestVideos/temp.png"
         image.save(temp_path)
         converter = Image2Ascii(temp_path)
         return converter.convert(convert_to_image=True, new_width=new_width, for_video=True)
 
-    def convert_video(self, fps=5, new_width=150):
+    def convert_video(self, fps: int = 5, new_width: int = 150):
+        '''
+        Converts video into Ascii Video
+        :param fps: Frames per Second
+        :param new_width:
+        '''
         frames = self.extract_frames(fps)
         image_paths = []
         for i, frame in enumerate(frames):
