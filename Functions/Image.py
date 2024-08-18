b05223521f38e0fb50203a2bb2d0dc6b1ca029dc
RobertArustamyan <robertarustamyan2@gmail.com>
2024-08-18 09:46:20 +0400

    Finished Image.py

--- Functions/Image.py
+++ Functions/Image.py
@@ -15,29 +15,51 @@ class Image2Ascii:
         except FileNotFoundError:
             print(f"There is no image in {path}")
 
-    def pixels_to_ascii(self) -> str:
+    def imageToAsciiString(self) -> str:
+        """
+        Converts image to ascii string.
+        :return: ascii string.
+        """
         pixels = self._image.getdata()
         characters = "".join([self.scale[pixel * (len(self.scale) - 1) // 255] for pixel in pixels])
         return characters
 
-    def convert_to_string(self, new_width: int = 200) -> str:
+    def convertToString(self, new_width: int = 200) -> str:
+        """
+        Adds '\n' to converted ascii string and makes it right sizes(width,height).
+        :param new_width: Width of image.
+        :return: Ascii string in correct sizes.
+        """
         self._image = self.resizeImage(self._image, new_width)
         self._image.convert('L')
-        new_image_str = self.pixels_to_ascii()
+        new_image_str = self.imageToAsciiString()
         pixel_count = len(new_image_str)
         ascii_string = '\n'.join(new_image_str[i:(i + new_width)] for i in range(0, pixel_count, new_width))
         return ascii_string
 
-    def convert(self, convert_to_image: bool, new_width: int = 200,save:bool = False,colorness=True):
+    def convert(self, convert_to_image: bool, new_width: int = 200,save:bool = False,colorness=True) -> None:
+        """
+        Converts an image depending on the parameters of function.
+        :param convert_to_image: True - converts to image. False - converts to string.
+        :param new_width: Width of Processed  Image.
+        :param save: True - saves converted image. False - doesn't save converted image(for video).
+        :param colorness: True - gives color to characters. False - characters will be in 182, 183, 183.
+        """
         if convert_to_image:
             return self.convertToColor(new_width, colorness=colorness, save=save)
         else:
-            ascii_str = self.convert_to_string(new_width)
+            ascii_str = self.convertToString(new_width)
             file_name = self.path.split('/')[-1].split('.')[0]
             with open(f"{"/".join(self.path.split('/')[:-1])}/{file_name}.txt", 'w') as f:
                 f.write(ascii_str)
     @staticmethod
-    def resizeImage(image: Image, newWidth: int):
+    def resizeImage(image: Image, newWidth: int) -> Image:
+        """
+        Counts ratio of height/width and converts image using new width.
+        :param image: Pillow image.
+        :param newWidth: Width of processed image.
+        :return: returns Resized image.
+        """
         width, height = image.size
         factor = height / width
         newHeight = int(factor * newWidth)
@@ -46,7 +68,16 @@ class Image2Ascii:
 
 
     def convertToColor(self, new_width: int = 200, font_size: int = 16, background: tuple = (40, 44, 52),
-                       colorness: bool = True, save: bool = False):
+                       colorness: bool = True, save: bool = False) -> Image:
+        """
+        Ascii character to image converter.
+        :param new_width: Width of processed image.
+        :param font_size: Size of character in image.
+        :param background: Background color in RGB format.
+        :param colorness: True - converts using colored characters. False - converts using gray characters.
+        :param save: True - saves an image. False - only returns PIL.Image.
+        :return: Pillow Image.
+        """
         # Resizing original image.
         originalImage = Image.open(self.path).convert('RGB')
         originalImage = self.resizeImage(originalImage,new_width)
